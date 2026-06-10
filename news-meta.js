dfeaf35c5e452ad7c4c0aa13504915b175c741ca
Michael Varnell <movarnell@gmail.com>
2026-06-10 15:03:30 -0500

    Add AI news update metadata

---
+++ news-meta.js
@@ -0,0 +1,5 @@
+window.newsFeedMeta = {
+  updatedAt: "2026-06-10T20:01:57Z",
+  displayUpdatedAt: "June 10, 2026 at 3:01 PM CT",
+  message: "Articles last updated June 10, 2026 at 3:01 PM CT.",
+};
