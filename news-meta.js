ea5af8a6923519b81e42a96f7ab056036e31048b
Michael Varnell <movarnell@gmail.com>
2026-07-30 08:05:07 -0500

    Refresh July 30 AI news timestamp

--- news-meta.js
+++ news-meta.js
@@ -1,7 +1,7 @@
 window.newsFeedMeta = {
-  updatedAt: "2026-07-28T08:03:00-05:00",
-  displayUpdatedAt: "July 28, 2026 at 8:03 AM CT",
-  message: "Articles last updated July 28, 2026 at 8:03 AM CT.",
+  updatedAt: "2026-07-30T08:02:00-05:00",
+  displayUpdatedAt: "July 30, 2026 at 8:02 AM CT",
+  message: "Articles last updated July 30, 2026 at 8:02 AM CT.",
 };
 
 (function renderNewsFeedMeta() {
